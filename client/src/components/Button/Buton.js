d167e9f861b33525c55651747508a23fe2b30891
David Ioana <dvdioana@gmail.com>
2025-04-08 18:55:08 +0200

    Add google logo to button and styles

--- client/src/components/Button/Buton.js
+++ client/src/components/Button/Buton.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-function Button( {type = "button", onClick = () => {}, name = "Button", className = ""} ) {
+function Button( {type = "button", onClick = () => {}, name = "Button", className = "", children} ) {
     return(
         <button 
             className={ `${styles.button} ${className}`} 
             type={type} 
             onClick={onClick}
         >   
-         {name}
+         {children || name}
         </button>
     );
 }
