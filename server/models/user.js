ffb390ae3b196dc71e44a6c5ed85bf2f4bf0ee82
David Ioana <dvdioana@gmail.com>
2025-05-09 15:19:18 +0200

    Add user profile helpers
    Implemented functions to find user by ID,  get followers, following and memories count, update user's bio and image

--- server/models/user.js
+++ server/models/user.js
@@ -37,6 +37,19 @@ async function findUserByEmail(email){
     }
 }
 
+async function findUserById(id){
+    try{
+        const result = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+        if(result.rows.length){
+            return result.rows[0];
+        }
+
+        return null;  
+    }catch(err){
+        console.error("Error finding user by id: ", err);
+    }
+}
+
 async function findUserByGoogleId(googleId){
     try{
         const result = await db.query('SELECT * FROM users WHERE google_id = $1', [googleId]);
@@ -64,4 +77,56 @@ async function comparePasswords(password, hashedPassword){
     return await bcrypt.compare(password, hashedPassword);
 }
 
-export {createUser, findUserByEmail, findUserByGoogleId, comparePasswords, deleteUser};
+async function getFollowersCount(userId){
+    try{
+        const result =  await db.query('SELECT COUNT(*) FROM followers WHERE following_id = $1', [userId]);
+        return result.rows[0].count;
+    }catch(err){
+        console.error('Error getting followers count: ', err);
+        throw new Error('Error getting followers count');
+    }
+}
+
+async function getFollowingCount(userId){
+    try{
+        const result = await db.query('SELECT COUNT(*) FROM followers WHERE follower_id = $1', [userId]);
+        return result.rows[0].count;
+    }catch(err){
+        console.error('Error getting following count: ', err);
+        throw new Error('Error getting following count');
+    }
+}
+
+async function getMemoriesCount(userId){
+    try{
+        const result = await db.query('SELECT COUNT(*) FROM memories WHERE user_id = $1', [userId]);
+        return result.rows[0].count;
+    }catch(err){
+        console.error('Error getting memories count: ', err);
+        throw new Error('Error getting memories count');
+    }
+}
+
+async function updateUserProfile(userId, {bio, image_url}){
+    const fields = [];
+    const values = [];
+    let index = 1;
+    if(bio){
+        fields.push(`bio = $${index++}`);
+        values.push(bio);
+    }
+
+    if(image_url){
+        fields.push(`image_url = $${index++}`);
+        values.push(image_url);
+    }
+
+    if(fields.length === 0) return;
+
+    const query = ` UPDATE users SET ${fields.join(', ')} WHERE id = $${index}`;
+    values.push(userId);
+
+    await db.query(query, values);
+
+}
+export {createUser, findUserByEmail, findUserByGoogleId, findUserById, comparePasswords, deleteUser, getFollowersCount, getFollowingCount, getMemoriesCount, updateUserProfile};
